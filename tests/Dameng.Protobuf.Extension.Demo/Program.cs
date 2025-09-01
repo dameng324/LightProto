955cc759ce428dce63c9f81b4367a5cb997a59dd
Dameng <313880747@qq.com>
2025-09-01 12:18:02 +0800

    Update demo project and CI workflows for protobuf changes
    Refined the .NET CI and publish workflows to target the test project directly and adjusted version handling. Updated the demo project to use the correct protobuf C# namespace, added Grpc.Tools as a development dependency, and improved the Program.cs example to demonstrate serialization and deserialization using the new namespace. These changes improve clarity and ensure the demo aligns with the intended protobuf usage.

--- tests/Dameng.Protobuf.Extension.Demo/Program.cs
+++ tests/Dameng.Protobuf.Extension.Demo/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Dameng.Protobuf.Extension;
+using Google.Protobuf;
 
-Console.WriteLine("Hello, World!");
+TestPackage.Request request = new TestPackage.Request()
+{
+    Message = "Hello, World!"
+};
+
+byte[] data = Serialize(request);
+var deserializedRequest = Deserialize<TestPackage.Request>(data);
+
+Console.WriteLine(deserializedRequest.Message);
+
+T Deserialize<T>(byte[] bytes) where T : IPbMessageParser<T>
+{
+    return T.Parser.ParseFrom(bytes);
+}
+byte[] Serialize<T>(T t) where T : IPbMessageParser<T>
+{
+    return t.ToByteArray();
+}
