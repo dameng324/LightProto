04c9c6d822c369f8641e860f53f608b665f3021d
Dameng <313880747@qq.com>
2025-10-10 20:53:11 +0800

    Consolidate collection and dictionary serializer files
    Merged serializer and deserializer logic for collections and dictionaries into single files (Serializer.Collection.cs and Serializer.Dictionary.cs). Removed redundant serializer files to simplify code organization and maintenance.

--- src/LightProto/Serializer.Collection.cs
+++ src/LightProto/Serializer.Collection.cs
@@ -41,7 +41,36 @@ public static partial class Serializer
             source,
             GetCollectionMessageReader<TCollection, TItem>(TItem.ProtoReader)
         );
+
+    /// <summary>
+    /// Writes a protocol-buffer representation of the given instance to the supplied stream.
+    /// </summary>
+    /// <param name="instance">The existing instance to be serialized (cannot be null).</param>
+    /// <param name="destination">The destination stream to write to.</param>
+    public static void Serialize<T>(Stream destination, ICollection<T> instance)
+        where T : IProtoParser<T> =>
+        Serialize(destination, instance, T.ProtoWriter.GetCollectionWriter());
+
+    /// <summary>
+    /// Writes a protocol-buffer representation of the given instance to the supplied writer.
+    /// </summary>
+    /// <param name="instance">The existing instance to be serialized (cannot be null).</param>
+    /// <param name="destination">The destination stream to write to.</param>
+    public static void Serialize<T>(IBufferWriter<byte> destination, ICollection<T> instance)
+        where T : IProtoParser<T> =>
+        Serialize(destination, instance, T.ProtoWriter.GetCollectionWriter());
 #endif
+
+    public static IProtoWriter<ICollection<T>> GetCollectionWriter<T>(this IProtoWriter<T> writer)
+    {
+        uint tag = WireFormat.MakeTag(1, writer.WireType);
+        return new IEnumerableProtoWriter<ICollection<T>, T>(
+            writer,
+            tag,
+            (collection) => collection.Count,
+            itemFixedSize: 0
+        );
+    }
 
     public static IEnumerableProtoReader<TCollection, TItem> GetCollectionReader<
         TCollection,
--- src/LightProto/Serializer.Dictionary.Serializer.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Buffers;
-using LightProto.Parser;
-
-namespace LightProto;
-
-public static partial class Serializer { }
