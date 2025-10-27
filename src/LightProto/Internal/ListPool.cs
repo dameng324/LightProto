f601b98a367cf353c0746448864311955f61bcf9
Dameng <313880747@qq.com>
2025-10-27 14:27:49 +0800

    Add List pooling for ArrayProtoReader performance
    Introduces SimpleObjectPool for pooling List<TItem> instances in ArrayProtoReader to reduce allocations and improve performance. Updates ArrayProtoReader to use the pool when reading arrays. Adds Microsoft.Extensions.ObjectPool package reference.

---
+++ src/LightProto/Internal/ListPool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace LightProto.Internal;
+
+internal class SimpleObjectPool<T>
+    where T : class
+{
+    private readonly ConcurrentBag<T> _objects;
+    private readonly Func<T> _objectFactory;
+    private readonly Action<T>? _resetAction;
+
+    public SimpleObjectPool(Func<T> objectFactory, Action<T>? resetAction = null)
+    {
+        _objectFactory = objectFactory ?? throw new ArgumentNullException(nameof(objectFactory));
+        _resetAction = resetAction;
+
+        _objects = new ConcurrentBag<T>();
+    }
+
+    public T Get()
+    {
+        if (_objects.TryTake(out var item))
+        {
+            return item;
+        }
+
+        return _objectFactory();
+    }
+
+    public void Return(T item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentNullException(nameof(item));
+        }
+
+        _resetAction?.Invoke(item);
+        _objects.Add(item);
+    }
+}
