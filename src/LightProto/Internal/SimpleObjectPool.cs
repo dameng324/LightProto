383051b228ea3c7837b9ab43ea95127bd4964426
Dameng <313880747@qq.com>
2025-10-27 15:01:02 +0800

    Rename ListPool to SimpleObjectPool and add tests
    Renamed ListPool.cs to SimpleObjectPool.cs and updated implementation to support a configurable max pool size. Added unit tests for SimpleObjectPool covering construction, object retrieval, return behavior, max size enforcement, and thread safety.

--- src/LightProto/Internal/SimpleObjectPool.cs
+++ src/LightProto/Internal/SimpleObjectPool.cs
@@ -9,11 +9,13 @@ internal class SimpleObjectPool<T>
     private readonly ConcurrentBag<T> _objects;
     private readonly Func<T> _objectFactory;
     private readonly Action<T>? _resetAction;
+    private readonly int _maxSize;
 
-    public SimpleObjectPool(Func<T> objectFactory, Action<T>? resetAction = null)
+    public SimpleObjectPool(Func<T> objectFactory, Action<T>? resetAction = null, int maxSize = 128)
     {
         _objectFactory = objectFactory ?? throw new ArgumentNullException(nameof(objectFactory));
         _resetAction = resetAction;
+        _maxSize = maxSize;
 
         _objects = new ConcurrentBag<T>();
     }
@@ -36,6 +38,11 @@ internal class SimpleObjectPool<T>
         }
 
         _resetAction?.Invoke(item);
+        if (_maxSize > 0 && _objects.Count >= _maxSize)
+        {
+            // discard
+            return;
+        }
         _objects.Add(item);
     }
 }
