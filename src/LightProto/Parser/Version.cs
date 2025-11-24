97d77306fe8f266babbae3ecd90b4231cfa7a92d
Dameng <313880747@qq.com>
2025-11-24 11:24:01 +0800

    Add `System.Version` serialization support and tests
    Introduces VersionProtoParser for serializing System.Version using protobuf. Adds corresponding tests and proto definitions to ensure correct serialization and deserialization of Version properties.

---
+++ src/LightProto/Parser/Version.cs
@@ -0,0 +1,53 @@
+﻿namespace LightProto.Parser;
+
+[ProtoContract]
+[ProtoSurrogateFor<Version>]
+public partial struct VersionProtoParser
+{
+    [ProtoMember(1, DataFormat = DataFormat.ZigZag)]
+    public int Major { get; set; }
+
+    [ProtoMember(2, DataFormat = DataFormat.ZigZag)]
+    public int Minor { get; set; }
+
+    [ProtoMember(3, DataFormat = DataFormat.ZigZag)]
+    public int Build { get; set; }
+
+    [ProtoMember(4, DataFormat = DataFormat.ZigZag)]
+    public int Revision { get; set; }
+
+    public static implicit operator Version(VersionProtoParser protoParser)
+    {
+        if (protoParser.Revision == -1)
+        {
+            if (protoParser.Build == -1)
+            {
+                return new Version(protoParser.Major, protoParser.Minor);
+            }
+            else
+            {
+                return new Version(protoParser.Major, protoParser.Minor, protoParser.Build);
+            }
+        }
+        else
+        {
+            return new Version(
+                protoParser.Major,
+                protoParser.Minor,
+                protoParser.Build,
+                protoParser.Revision
+            );
+        }
+    }
+
+    public static implicit operator VersionProtoParser(Version value)
+    {
+        return new VersionProtoParser
+        {
+            Major = value.Major,
+            Minor = value.Minor,
+            Build = value.Build,
+            Revision = value.Revision,
+        };
+    }
+}
---
+++ tests/LightProto.Tests/Parsers/Version.cs
@@ -0,0 +1,58 @@
+﻿using LightProto;
+using LightProto.Parser;
+
+namespace LightProto.Tests.Parsers;
+
+[InheritsTests]
+public partial class VersionTests : BaseTests<VersionTests.Message, VersionTestsMessage>
+{
+    [ProtoContract]
+    [ProtoBuf.ProtoContract]
+    public partial record Message
+    {
+        [ProtoMember(1)]
+        [ProtoBuf.ProtoMember(1)]
+        public Version Property { get; set; }
+    }
+
+    protected override bool ProtoBuf_net_Serialize_Disabled { get; } = true;
+    protected override bool ProtoBuf_net_Deserialize_Disabled { get; } = true;
+
+    public override IEnumerable<Message> GetMessages()
+    {
+        yield return new() { Property = new Version() };
+        yield return new() { Property = new Version(1, 2) };
+        yield return new() { Property = new Version(1, 2, 3) };
+        yield return new() { Property = new Version(1, 2, 3, 4) };
+        yield return new() { Property = new Version("1.2.3.4") };
+    }
+
+    public override IEnumerable<VersionTestsMessage> GetGoogleMessages()
+    {
+        return GetMessages()
+            .Select(o => new VersionTestsMessage
+            {
+                Property = new BclVersion()
+                {
+                    Major = o.Property.Major,
+                    Minor = o.Property.Minor,
+                    Build = o.Property.Build,
+                    Revision = o.Property.Revision
+                }
+            });
+    }
+
+    public override async Task AssertResult(Message clone, Message message)
+    {
+        await Assert.That(clone.Property).IsEquivalentTo(message.Property);
+        //await Assert.That(clone.Property.Kind).IsEquivalentTo(message.Property.Kind); // Kind is not include by default in protobuf-net
+    }
+
+    public override async Task AssertGoogleResult(VersionTestsMessage clone, Message message)
+    {
+        await Assert.That(clone.Property.Major).IsEquivalentTo(message.Property.Major);
+        await Assert.That(clone.Property.Minor).IsEquivalentTo(message.Property.Minor);
+        await Assert.That(clone.Property.Build).IsEquivalentTo(message.Property.Build);
+        await Assert.That(clone.Property.Revision).IsEquivalentTo(message.Property.Revision);
+    }
+}
