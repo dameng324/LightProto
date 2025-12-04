b91a40b7fb100d9335eb4273210fdc0b79cc5285
Dameng <313880747@qq.com>
2025-12-04 08:30:17 +0800

    Add Vector2,Vector3,Quaternion,Plane,Matrix3x2,Matrix4x4 Support (#118)
    * Add parsers and tests for System.Numerics types
    
    Introduces ProtoBuf surrogate parsers for Vector2, Vector3, Quaternion, Plane, Matrix3x2, and Matrix4x4 in LightProto.Parser. Adds corresponding test cases and updates parser.proto with new message definitions for these types and their test wrappers.
    
    * Update CollectionTests.cs
    
    * Add validation for protoParser float array length
    
    Added null and length checks to implicit conversion operators for Matrix3x2, Matrix4x4, Plane, and Quaternion proto parsers. If the float array is null, the default struct is returned; if the array length is invalid, an ArgumentException is thrown. Corresponding unit tests were added to verify these behaviors.

---
+++ tests/LightProto.Tests/Parsers/Matrix4x4.cs
@@ -0,0 +1,134 @@
+﻿#if NET7_0_OR_GREATER
+using System.Buffers.Binary;
+using System.Numerics;
+using Google.Protobuf;
+using LightProto;
+using LightProto.Parser;
+
+namespace LightProto.Tests.Parsers;
+
+[InheritsTests]
+public partial class Matrix4x4Tests : BaseTests<Matrix4x4Tests.Message, Matrix4x4TestsMessage>
+{
+    [ProtoContract]
+    public partial record Message
+    {
+        [ProtoMember(1)]
+        public Matrix4x4 Property { get; set; }
+    }
+
+    public override IEnumerable<Matrix4x4TestsMessage> GetGoogleMessages()
+    {
+        return GetMessages()
+            .Select(x => new Matrix4x4TestsMessage()
+            {
+                Property = new Matrix4x4Message()
+                {
+                    M =
+                    {
+                        x.Property.M11,
+                        x.Property.M12,
+                        x.Property.M13,
+                        x.Property.M14,
+                        x.Property.M21,
+                        x.Property.M22,
+                        x.Property.M23,
+                        x.Property.M24,
+                        x.Property.M31,
+                        x.Property.M32,
+                        x.Property.M33,
+                        x.Property.M34,
+                        x.Property.M41,
+                        x.Property.M42,
+                        x.Property.M43,
+                        x.Property.M44,
+                    },
+                },
+            });
+    }
+
+    protected override bool ProtoBuf_net_Deserialize_Disabled { get; } = true;
+    protected override bool ProtoBuf_net_Serialize_Disabled { get; } = true;
+
+    public override IEnumerable<Message> GetMessages()
+    {
+        yield return new()
+        {
+            Property = new Matrix4x4(-1, -2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+        };
+        yield return new()
+        {
+            Property = new Matrix4x4(1, -2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+        };
+        yield return new()
+        {
+            Property = new Matrix4x4(-1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+        };
+        yield return new()
+        {
+            Property = new Matrix4x4(1, 2, -3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+        };
+        yield return new()
+        {
+            Property = new Matrix4x4(0, 0, 0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
+        };
+    }
+
+    public override async Task AssertResult(Message clone, Message message)
+    {
+        await Assert.That(clone.Property).IsEqualTo(message.Property);
+    }
+
+    public override async Task AssertGoogleResult(Matrix4x4TestsMessage clone, Message message)
+    {
+        await Assert.That(clone.Property.M.Count).IsEqualTo(16);
+        var matrix = new Matrix4x4(
+            clone.Property.M[0],
+            clone.Property.M[1],
+            clone.Property.M[2],
+            clone.Property.M[3],
+            clone.Property.M[4],
+            clone.Property.M[5],
+            clone.Property.M[6],
+            clone.Property.M[7],
+            clone.Property.M[8],
+            clone.Property.M[9],
+            clone.Property.M[10],
+            clone.Property.M[11],
+            clone.Property.M[12],
+            clone.Property.M[13],
+            clone.Property.M[14],
+            clone.Property.M[15]
+        );
+        await Assert.That(matrix).IsEqualTo(message.Property);
+    }
+
+    [Test]
+    public async Task NullFloatsArray_Should_ParseToDefault()
+    {
+        var protoParser = new Matrix4x4ProtoParser() { Floats = null };
+
+        Matrix4x4 result = protoParser;
+        await Assert.That(result).IsEqualTo(default(Matrix4x4));
+    }
+
+    [Test]
+    public async Task FloatsArrayWithInvalidLength_Should_ThrowException()
+    {
+        var protoParser = new Matrix4x4ProtoParser()
+        {
+            Floats = new float[] { 1, 2, 3 }, // Invalid length
+        };
+
+        var exception = await Assert
+            .That(() =>
+            {
+                Matrix4x4 result = protoParser;
+            })
+            .Throws<ArgumentException>();
+        await Assert
+            .That(exception!.Message)
+            .Contains("Input array must contain 16 elements for Matrix4x4 conversion.");
+    }
+}
+#endif
---
+++ tests/LightProto.Tests/Parsers/Quaternion.cs
@@ -0,0 +1,89 @@
+﻿#if NET7_0_OR_GREATER
+using System.Buffers.Binary;
+using System.Numerics;
+using Google.Protobuf;
+using LightProto;
+using LightProto.Parser;
+
+namespace LightProto.Tests.Parsers;
+
+[InheritsTests]
+public partial class QuaternionTests : BaseTests<QuaternionTests.Message, QuaternionTestsMessage>
+{
+    [ProtoContract]
+    public partial record Message
+    {
+        [ProtoMember(1)]
+        public Quaternion Property { get; set; }
+    }
+
+    public override IEnumerable<QuaternionTestsMessage> GetGoogleMessages()
+    {
+        return GetMessages()
+            .Select(x => new QuaternionTestsMessage()
+            {
+                Property = new QuaternionMessage()
+                {
+                    M = { x.Property.X, x.Property.Y, x.Property.Z, x.Property.W },
+                },
+            });
+    }
+
+    protected override bool ProtoBuf_net_Deserialize_Disabled { get; } = true;
+    protected override bool ProtoBuf_net_Serialize_Disabled { get; } = true;
+
+    public override IEnumerable<Message> GetMessages()
+    {
+        yield return new() { Property = new Quaternion(-1, -2, 3, 4) };
+        yield return new() { Property = new Quaternion(1, -2, 3, 4) };
+        yield return new() { Property = new Quaternion(-1, 2, 3, 4) };
+        yield return new() { Property = new Quaternion(1, 2, -3, 4) };
+        yield return new() { Property = new Quaternion(0, 0, 0, 4) };
+    }
+
+    public override async Task AssertResult(Message clone, Message message)
+    {
+        await Assert.That(clone.Property.X).IsEqualTo(message.Property.X);
+        await Assert.That(clone.Property.Y).IsEqualTo(message.Property.Y);
+        await Assert.That(clone.Property.Z).IsEqualTo(message.Property.Z);
+        await Assert.That(clone.Property.W).IsEqualTo(message.Property.W);
+    }
+
+    public override async Task AssertGoogleResult(QuaternionTestsMessage clone, Message message)
+    {
+        await Assert.That(clone.Property.M.Count).IsEqualTo(4);
+        await Assert.That(clone.Property.M[0]).IsEqualTo(message.Property.X);
+        await Assert.That(clone.Property.M[1]).IsEqualTo(message.Property.Y);
+        await Assert.That(clone.Property.M[2]).IsEqualTo(message.Property.Z);
+        await Assert.That(clone.Property.M[3]).IsEqualTo(message.Property.W);
+    }
+
+    [Test]
+    public async Task NullFloatsArray_Should_ParseToDefault()
+    {
+        var protoParser = new QuaternionProtoParser() { Floats = null };
+
+        Quaternion result = protoParser;
+        await Assert.That(result).IsEqualTo(default(Quaternion));
+    }
+
+    [Test]
+    public async Task FloatsArrayWithInvalidLength_Should_ThrowException()
+    {
+        var protoParser = new QuaternionProtoParser()
+        {
+            Floats = new float[] { 1, 2, 3 }, // Invalid length
+        };
+
+        var exception = await Assert
+            .That(() =>
+            {
+                Quaternion result = protoParser;
+            })
+            .Throws<ArgumentException>();
+        await Assert
+            .That(exception!.Message)
+            .Contains("Input array must contain 4 elements for Quaternion conversion.");
+    }
+}
+#endif
