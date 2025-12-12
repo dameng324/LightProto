98ee3edf9eca54dd3dfeae2a30ad5f7d623c4fef
Dameng <313880747@qq.com>
2025-12-12 22:08:02 +0800

    Add ReadOnlyDictionary and SortedSet support  (#121)
    * Add ReadOnlyDictionary and SortedSet support to parser
    
    Introduces ReadOnlyDictionaryProtoReader/Writer and SortedSetProtoReader/Writer for LightProto. Adds corresponding tests to ensure correct serialization and deserialization of ReadOnlyDictionary and SortedSet types. Also updates test classes to enable ProtoBuf-net serialization for relevant collections.
    
    * Update ReadOnlyDictionary.cs

---
+++ tests/LightProto.Tests/Parsers/SortedSet.cs
@@ -0,0 +1,52 @@
+﻿using LightProto;
+
+namespace LightProto.Tests.Parsers;
+
+[InheritsTests]
+public partial class SortedSetPackedTests
+    : BaseTests<SortedSetPackedTests.Message, ISetPackedTestsMessage>
+{
+    [ProtoContract]
+    [ProtoBuf.ProtoContract]
+    public partial record Message
+    {
+        [ProtoMember(1)]
+        [ProtoBuf.ProtoMember(1)]
+        public required SortedSet<int> Property { get; set; } = new();
+
+        public override string ToString()
+        {
+            return $"Property: {string.Join(", ", Property)}";
+        }
+    }
+
+    public override IEnumerable<Message> GetMessages()
+    {
+        yield return new()
+        {
+            Property = new SortedSet<int>() { 1, 2, 3, 4, 5 },
+        };
+        yield return new()
+        {
+            Property = new SortedSet<int>() { -1, -2, -3, -4, -5 },
+        };
+        yield return new() { Property = new SortedSet<int>() { 0 } };
+        yield return new() { Property = new SortedSet<int>() { } };
+    }
+
+    public override async Task AssertResult(Message clone, Message message)
+    {
+        await Assert.That(clone.Property).IsEquivalentTo(message.Property);
+    }
+
+    public override IEnumerable<ISetPackedTestsMessage> GetGoogleMessages()
+    {
+        return GetMessages()
+            .Select(o => new ISetPackedTestsMessage() { Property = { o.Property } });
+    }
+
+    public override async Task AssertGoogleResult(ISetPackedTestsMessage clone, Message message)
+    {
+        await Assert.That(clone.Property.ToArray()).IsEquivalentTo(message.Property.ToArray());
+    }
+}
