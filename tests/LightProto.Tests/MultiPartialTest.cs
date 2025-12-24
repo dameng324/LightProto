91f2e39da7341fe71e131e700314a53cad4ebf1e
Dameng <313880747@qq.com>
2025-12-24 16:13:17 +0800

    add multi-partial class support
    Refactored LightProtoGenerator to use ForAttributeWithMetadataName for improved attribute detection and code clarity. Removed the obsolete PbMessageParserGenerator. Added a new test for multi-partial classes to ensure correct serialization/deserialization. Updated TestAot/Program.cs to use partial classes and MemoryPack.

---
+++ tests/LightProto.Tests/MultiPartialTest.cs
@@ -0,0 +1,35 @@
+﻿namespace LightProto.Tests;
+
+[ProtoContract]
+public partial class MultiPartialTestMessage
+{
+    [ProtoMember(1)]
+    public int Part1 { get; set; }
+}
+
+partial class MultiPartialTestMessage
+{
+    [ProtoMember(2)]
+    public string Part2 { get; set; } = "";
+}
+
+public class MultiPartialTest
+{
+    [Test]
+    public async Task MultiPartialClass_Should_SerializeAndDeserializeCorrectly()
+    {
+        var message = new MultiPartialTestMessage() { Part1 = 42, Part2 = "Hello, World!" };
+
+#if NET7_0_OR_GREATER
+        var cloned = Serializer.DeepClone(message);
+#else
+        var cloned = Serializer.DeepClone(
+            message,
+            MultiPartialTestMessage.ProtoReader,
+            MultiPartialTestMessage.ProtoWriter
+        );
+#endif
+        await Assert.That(cloned.Part1).IsEqualTo(message.Part1);
+        await Assert.That(cloned.Part2).IsEqualTo(message.Part2);
+    }
+}
